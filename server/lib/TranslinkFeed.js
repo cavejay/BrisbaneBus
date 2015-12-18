678ec2aa96c07a91cd69251f3868cd08f46c1db6
Michael Ball <michael@sablecliff.com>
2015-12-18 13:32:37 +1000

    basic server done with translink module looking good

--- server/lib/TranslinkFeed.js
+++ server/lib/TranslinkFeed.js
@@ -1,27 +1,62 @@
 var Protobuf = require('protobufjs');
+var util = require('util');
+var request = require('request');
+var EventEmitter = require('events').EventEmitter;
 
-var Feed = {
-	gtfsrt: {},
-	protoDesc: {}
+var requestStructure = {
+  method: 'GET',
+  url: undefined,
+  encoding: null
 };
 
+function Feed () {
+  // if user accidentally omits the new keyword, this will silently correct the problem...
+  if (!(this instanceof Feed)) return new Feed();
+
+  EventEmitter.call(this);
+}
+
+Feed.gtfsrt = {};
+Feed.protoDesc = {};
+Feed.lastKnownGood = {
+  time: 0,
+  feed: undefined
+};
+
+util.inherits(Feed, EventEmitter);
+
+/* Loads the protobuffer and does an initial fetching of the realtime feed
+ */
 Feed.setup = function (feedURL, protoFile) {
-  if (!protoFile) protoFile = 'resources/gtfs-realtime.proto';
+  if (!feedURL) throw new Error('Invalid feedURL');
+  if (!protoFile) protoFile = '../resources/gtfs-realtime.proto';
   this.protoDesc = Protobuf.loadProtoFile(protoFile);
 
-  if (!this.protoDesc) throw new Error('Could not create Protobuffer from file');
+  if (!this.protoDesc) throw new Error('ProtoBuffer Description Loading Failed');
 
   this.gtfsrt = this.protoDesc.build().transit_realtime;
-  if (this.gtfsrt) console.log('gtfsrt:\n' + util.inspect(gtfsrt));
-  else {
-    console.log('things failed :(');
-    process.exit();
-  }
+  if (!this.gtfsrt) throw new Error('ProtoBuff Build Failed');
+
+  requestStructure.url = feedURL;
+
+  Feed.update();
   return this;
 };
 
+/* Fetches the realtime feed and stores the current time and the decoded feed
+ * in Feed.lastKnownGood
+ */
 Feed.update = function () {
-
+  request(requestStructure, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      Feed.lastKnownGood.feed = this.gtfsrt.FeedMessage.decode(body);
+      Feed.lastKnownGood.time = new Date();
+    }
+    Feed.emit('updated');
+  });
 };
 
-exports = module.export = Feed;
+Feed.getRoute = function (routeId) {
+}
+
+exports = module.export = new Feed();
--- server/main.js
+++ server/main.js
@@ -1,23 +1,30 @@
 // this is main
-var process = require('process');
-var request = require('request');
 var restify = require('restify');
 var util = require('util');
 var translink = require('./lib/TranslinkGTFS.js');
-var feed = require('./lib/TranslinkFeed.js');
+var feed = require('./lib/TranslinkFeed.js').setup(translink.gtfsrt);
 
-var requestSettings = {
-  method: 'GET',
-  url: translink.gtfsrt,
-  encoding: null
-};
-
-request(requestSettings, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    var feed = gtfsrt.FeedMessage.decode(body);
-    console.log('passed');
-    console.log(util.inspect(feed));
-  } else {
-    console.log('failed');
-  }
+var server = restify.createServer({
+  name: 'BrisbaneBus'
 });
+
+server.get('/hello/:name', function (req, res, next) {
+  res.send('Hello ' + req.params.name);
+});
+
+server.get('/route/:routeid', function (req, res, next) {
+  res.send('Getting route: ' + req.params.routeid);
+  feed.update();
+});
+
+server.get('/routelist', function (req, res, next) {
+  res.send('Getting list of Buses');
+});
+
+server.listen(8080, function () {
+  console.log('%s listening at %s', server.name, server.url);
+});
+
+feed.on('updated', function () {
+  
+})
